88a32c4ab662534e441b921cde9e901f982ed679
hoseinhabibiyan <hn.habibiyan@gmail.com>
2023-08-13 11:08:14 +0330

    add employee work report

--- HR/views.py
+++ HR/views.py
@@ -7,6 +7,15 @@ from .models.employee_work_report import EmployeeWorkReport
 def home(request):
     return render(request, 'home.html')
 
+def add_employee_work_report(request):
+    if request.method == 'POST':
+        form = EmployeeWorkReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    form = EmployeeWorkReportForm()
+    return render(request, 'employee_work_report.html', {'form': form})
+
 def report(request):
     form = ReportForm()
     return render(request, 'report.html', {'form': form})
